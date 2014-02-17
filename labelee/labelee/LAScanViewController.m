a4fac6848629cc7768b3a69f666a87393e99c887
Michel Barbou Salvador <michel@bizeulabs.com>
2014-02-17 14:26:26 +0100

    Dibujados los recuadros de enfoque

--- labelee/labelee/LAScanViewController.m
+++ labelee/labelee/LAScanViewController.m
@@ -93,6 +93,51 @@
     [instructionBar addSubview:self.instructionLabel];
     
     
+    //Para la esquina inferior izquierda
+    UIView *verticalRectangleLB = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.origin.x + 10, self.view.frame.size.height - 66,6, 20)];
+//    verticalRectangleLB.backgroundColor = [UIColor colorWithRed:(246.0 / 255.0) green:(140.0 / 255.0) blue:(7.0 / 255.0) alpha:1];
+    verticalRectangleLB.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:verticalRectangleLB];
+    
+    UIView *horizontalRectangleLB = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.origin.x + 10, self.view.frame.size.height - 46,26, 6)];
+//    horizontalRectangleLB.backgroundColor = [UIColor colorWithRed:(246.0 / 255.0) green:(140.0 / 255.0) blue:(7.0 / 255.0) alpha:1];
+    horizontalRectangleLB.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:horizontalRectangleLB];
+    
+    //Para la esquina inferior derecha
+    UIView *verticalRectangleRB = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.size.width - 16, self.view.frame.size.height - 66,6, 20)];
+//    verticalRectangleRB.backgroundColor = [UIColor colorWithRed:(18.0 / 255.0) green:(147.0 / 255.0) blue:(205.0 / 255.0) alpha:1];
+    verticalRectangleRB.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:verticalRectangleRB];
+    
+    UIView *horizontalRectangleRB = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.size.width - 36, self.view.frame.size.height - 46,26, 6)];
+//    horizontalRectangleRB.backgroundColor = [UIColor colorWithRed:(18.0 / 255.0) green:(147.0 / 255.0) blue:(205.0 / 255.0) alpha:1];
+    horizontalRectangleRB.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:horizontalRectangleRB];
+    
+    //Para la esquina superior izquierda
+    UIView *verticalRectangleLT = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.origin.x + 10, self.view.frame.origin.y + 80,6, 20)];
+//    verticalRectangleLT.backgroundColor = [UIColor colorWithRed:(207.0 / 255.0) green:(2.0 / 255.0) blue:(1.0 / 255.0) alpha:1];
+    verticalRectangleLT.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:verticalRectangleLT];
+    
+    UIView *horizontalRectangleLT = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.origin.x + 10, self.view.frame.origin.y + 74,26, 6)];
+//    horizontalRectangleLT.backgroundColor = [UIColor colorWithRed:(207.0 / 255.0) green:(2.0 / 255.0) blue:(1.0 / 255.0) alpha:1];
+    horizontalRectangleLT.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:horizontalRectangleLT];
+    
+    //Para la esquina superior derecha
+    UIView *verticalRectangleRT = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.size.width - 16, self.view.frame.origin.y + 80,6, 20)];
+//    verticalRectangleRT.backgroundColor = [UIColor colorWithRed:(246.0 / 255.0) green:(140.0 / 255.0) blue:(7.0 / 255.0) alpha:1];
+    verticalRectangleRT.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:verticalRectangleRT];
+    
+    UIView *horizontalRectangleRT = [[UIView alloc]initWithFrame:CGRectMake(self.view.frame.size.width - 36, self.view.frame.origin.y + 74,26, 6)];
+//    horizontalRectangleRT.backgroundColor = [UIColor colorWithRed:(246.0 / 255.0) green:(140.0 / 255.0) blue:(7.0 / 255.0) alpha:1];
+    horizontalRectangleRT.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:horizontalRectangleRT];
+    
+    
 }
 
 - (void) viewDidAppear: (BOOL) animated
@@ -122,13 +167,13 @@
     
     NSLog(@"MOSTRAMOS EL MAPA SIN POSICIONAR");
     
-    NSURL *url = [NSURL URLWithString:@"http://labelee.com/appmobile"];
+    NSURL *url = [NSURL URLWithString:@"http://www.label.ee/appmobile"];
     
     // May return nil if a tracker has not already been initialized with a property
     // ID.
     id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
     
-    [tracker send:[[GAIDictionaryBuilder createEventWithCategory:@"Map_action" action:@"show_unlocated_map" label:@"http://labelee.com/appmobile" value:nil]build]];
+    [tracker send:[[GAIDictionaryBuilder createEventWithCategory:@"Map_action" action:@"show_unlocated_map" label:@"http://www.label.ee/appmobile" value:nil]build]];
     
     
     LABrowserViewController *browserVC = [[LABrowserViewController alloc]initWithURL:url];
